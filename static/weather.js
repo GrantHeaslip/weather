0b0e5b25a8262ab2218a090f2bb74b21239d4223
Grant Heaslip <grant@heaslip.me>
2018-06-24 22:53:26 -0400

    Add JS handler for city <select>

---
+++ static/weather.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Ensure that code runs as soon as possible, but no sooner
+if (document.readyState === 'interactive' || document.readyState === 'complete') {
+    onDocumentInteractive();
+} else {
+    document.onreadystatechange = function () {
+        if (document.readyState === 'interactive' || document.readyState === 'complete') {
+            document.onreadystatechange = null;
+            onDocumentInteractive();
+        }
+    };
+}
+
+function onDocumentInteractive() {
+    var citySelect = document.querySelector('.js-city-select');
+
+    citySelect.addEventListener(
+        'change',
+        function(_event) {
+            window.location.href = '/city/' + citySelect.value + '.html';
+        },
+        false
+    );
+}
